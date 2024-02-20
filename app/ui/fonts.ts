cb8cf04e74b02ab2ec1868fb998da712f0638912
asitrakah <sitraka@team-flow-corporation.com>
2024-02-20 15:46:19 +0400

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto, Montserrat, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
+export const monserrat = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
